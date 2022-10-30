7b5b7fff9568d9568619d4d7c1c3c3620a74adc9
gmartirosyan <grigor.martirosyan@jambit.com>
2022-10-30 21:27:58 +0400

    name chnage

---
+++ src/main/java/am/grig/deploy/controller/ApiController.java
@@ -0,0 +1,15 @@
+package am.grig.deploy.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public ResponseEntity<String > isHealthy(){
+        return ResponseEntity.ok("it works successfully");
+    }
+}
